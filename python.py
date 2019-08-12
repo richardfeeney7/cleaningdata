9fea0c4cfc6ab930bd8ad629156e3e41bad3e231
Richard Feeney <richardfeeney7@gmail.com>
2019-08-13 00:24:14 +0100

    Fist Commit

---
+++ python.py
@@ -0,0 +1,9 @@
+# Library Import
+    import numpy as np 
+    import pandas as pd 
+
+# read the csv file into pandas as dataframe
+    df = pd.read_csv("propertydata.csv.xlst")
+
+# Look at the data
+    df.head()
